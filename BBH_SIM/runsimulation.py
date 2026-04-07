f2eb769ed6ad7ed8fb5ff834d0004462f4dd958b
Synodii <chasmaticat@gmail.com>
2026-04-07 16:02:25 -0400

    added input param datasets and added a ton to runsimulation.
    add

--- BBH_SIM/runsimulation.py
+++ BBH_SIM/runsimulation.py
@@ -1,3 +1,45 @@
 import numpy as np
+import pandas as pd
+from pathlib import Path
 from .simulation import BBHSimulation
 from .datastorage import data_storage
+from .dynamics import compute_positions
+
+# ── Simulation time config (adjust as needed) ─────────────────────────────────
+T_START = 0.0        # seconds
+T_END   = 3.15576e10 # seconds (1000 yr)
+DT      = 1e4        # seconds
+
+#xlsx_path = r"InputParams/BHMergerSimulationParameters.xlsx"
+xlsx_path = r"InputParams/BHMergerSimulationParameters_constvel.xlsx"
+
+
+def load_parameters(xlsx_path: str | Path) -> pd.DataFrame:
+    return pd.read_excel(xlsx_path)
+
+def build_simulation(row: pd.Series) -> BBHSimulation:
+    #make compute_positions in dynamics to get r1 and r2 x and y components.
+    
+    return BBHSimulation(
+        m1      = float(row["BH1 Mass (M_sol)"]),
+        m2      = float(row["BH2 Mass (M_sol)"]),
+        
+        r1_init = r1_init,
+        r2_init = r2_init,
+        
+        v1_init = np.array([
+        float(row["BH1 Initial X Velocity (km/s)"]),
+        float(row["BH1 Initial Y Velocity (km/s)"]),
+    ])
+        v2_init = np.array([
+        float(row["BH2 Initial X Velocity (km/s)"]),
+        float(row["BH2 Initial Y Velocity (km/s)"]),
+    ])
+        
+        t_start = T_START,
+        t_end   = T_END,
+        dt      = DT,
+       
+def run_all (
+    
+    #ADD THIS LATER
