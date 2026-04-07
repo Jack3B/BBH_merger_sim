2ef9cc1d6cab89aa7b331cd2fce6208e5fab6b26
Synodii <chasmaticat@gmail.com>
2026-04-07 16:11:15 -0400

    more prep for runsimulation.py

--- BBH_SIM/runsimulation.py
+++ BBH_SIM/runsimulation.py
@@ -9,15 +9,19 @@ from .dynamics import compute_positions
 T_START = 0.0        # seconds
 T_END   = 3.15576e10 # seconds (1000 yr)
 DT      = 1e4        # seconds
+r2_init = np.array([10],[0]) #adjust as need be with preliminary tests
+r1_init_pre_param = np.array([0],[0]) #y gets adjusted by impact param
 
-#xlsx_path = r"InputParams/BHMergerSimulationParameters.xlsx"
-xlsx_path = r"InputParams/BHMergerSimulationParameters_constvel.xlsx"
+#inital pos for bh1 and bh2 then calc adjust for impact param for bh1
 
+#xlsx_path = r"InputParams/BHMergerSimulationParameters.xlsx" #111,100 sims
+xlsx_path = r"InputParams/BHMergerSimulationParameters_constvel.xlsx" #1111 sims lmao
 
 def load_parameters(xlsx_path: str | Path) -> pd.DataFrame:
     return pd.read_excel(xlsx_path)
 
 def build_simulation(row: pd.Series) -> BBHSimulation:
+    
     #make compute_positions in dynamics to get r1 and r2 x and y components.
     
     return BBHSimulation(
